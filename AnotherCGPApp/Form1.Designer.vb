647e5a07a493bcce4561896cba15c05546849841
enyiaja <enyiaja@gmail.com>
2017-10-26 16:07:06 +0100

    Initial commit

---
+++ AnotherCGPApp/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.txtNo = New System.Windows.Forms.TextBox()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Location = New System.Drawing.Point(12, 62)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(499, 204)
+        Me.Panel1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(523, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.ToolStripMenuItem2, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(111, 6)
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save As"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(111, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 268)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(523, 50)
+        Me.Panel2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Green
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(449, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(61, 27)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.txtNo)
+        Me.Panel3.Controls.Add(Me.btnRemove)
+        Me.Panel3.Controls.Add(Me.btnAdd)
+        Me.Panel3.Location = New System.Drawing.Point(11, 3)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(115, 42)
+        Me.Panel3.TabIndex = 3
+        '
+        'txtNo
+        '
+        Me.txtNo.Location = New System.Drawing.Point(30, 10)
+        Me.txtNo.Name = "txtNo"
+        Me.txtNo.Size = New System.Drawing.Size(53, 20)
+        Me.txtNo.TabIndex = 2
+        '
+        'btnRemove
+        '
+        Me.btnRemove.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRemove.Location = New System.Drawing.Point(4, 9)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(20, 20)
+        Me.btnRemove.TabIndex = 0
+        Me.btnRemove.Text = "-"
+        Me.btnRemove.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(89, 10)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(20, 20)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(523, 318)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MinimumSize = New System.Drawing.Size(539, 320)
+        Me.Name = "Form1"
+        Me.Text = "Solarized CGPA Calculator"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtNo As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Button1 As Button
+End Class
---
+++ AnotherCGPApp/Form1.vb
@@ -0,0 +1,217 @@
+﻿Imports System
+Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+
+Public Class Form1
+    Dim no As Integer = 0
+    Public Structure Course
+        Dim lblNo As Label
+        Dim txtName As TextBox
+        Dim cmbUnits As ComboBox
+        Dim txtScore As TextBox
+        Dim txtLetter As TextBox
+        Dim cmbGrade As ComboBox
+    End Structure
+    Dim x As Integer = 2
+    Dim y As Integer = 6
+    Dim x1 As Integer = 33
+    Dim x2 As Integer = 223
+    Dim x3 As Integer = 274
+    Dim x4 As Integer = 343
+    Dim x5 As Integer = 399
+    Dim Run(no) As Course
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim lblNolbl As Label = New Label
+        lblNolbl.Text = "s/n"
+        lblNolbl.Location = New Point(x + 12, -24)
+
+        Dim txtNamelbl As Label = New Label
+        txtNamelbl.Text = "Course Title"
+        txtNamelbl.Location = New Point(x1 + 12, -24)
+
+        Dim cmbUnitslbl As Label = New Label
+        cmbUnitslbl.Text = "Course Units"
+        cmbUnitslbl.Location = New Point(x2 + 12, -24)
+
+        Dim txtScorelbl As Label = New Label
+        txtScorelbl.Text = "Score"
+        txtScorelbl.Location = New Point(x3 + 12, -24)
+
+        Dim cmbGradelbl As Label = New Label
+        cmbGradelbl.Text = "Score"
+        cmbGradelbl.Location = New Point(x4 + 12, -24)
+
+        Dim txtLetterlbl As Label = New Label
+        txtLetterlbl.Text = "Grade"
+        txtLetterlbl.Location = New Point(x5 + 12, -24)
+
+
+
+        With Run(0)
+            .lblNo = New Label
+            With .lblNo
+                .Location = New Point(x, -24)
+            End With
+            .txtName = New TextBox
+            With .txtName
+                .Location = New Point(x1, -24)
+            End With
+            .cmbUnits = New ComboBox
+            With .cmbUnits
+                .Location = New Point(x2, -24)
+            End With
+            .txtScore = New TextBox
+            With .txtScore
+                .Location = New Point(x3, -24)
+            End With
+            .cmbGrade = New ComboBox
+            With .cmbGrade
+                .Location = New Point(x4, -24)
+            End With
+            .txtLetter = New TextBox
+            With .txtLetter
+                .Location = New Point(x5, -24)
+            End With
+        End With
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        ' Button to Remove Last Course
+        Panel1.Controls.Remove(Run(no).lblNo)
+        Panel1.Controls.Remove(Run(no).txtName)
+        Panel1.Controls.Remove(Run(no).cmbUnits)
+        Panel1.Controls.Remove(Run(no).txtScore)
+        Panel1.Controls.Remove(Run(no).cmbGrade)
+        Panel1.Controls.Remove(Run(no).txtLetter)
+        no -= 1
+        txtNo.Text = no
+        ReDim Preserve Run(UBound(Run) - 1)
+        y -= 30
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Button to add a course
+        no += 1
+        txtNo.Text = no
+        'y += 30
+        ReDim Preserve Run(no + 1)
+        With Run(no)
+            .lblNo = New Label
+            With .lblNo
+                .Text = no
+                .Size = New Size(20, 13)
+
+                .Location = New Point(x, Run(no - 1).lblNo.Location.Y + 30)
+                .Parent = Panel1
+            End With
+
+            .txtName = New TextBox
+            With .txtName
+                .Size = New Size(174, 20)
+                .Location = New Point(x1, Run(no - 1).txtName.Location.Y + 30)
+                .Parent = Panel1
+            End With
+
+            .cmbUnits = New ComboBox
+            With .cmbUnits
+                For i = 1 To 6
+                    .Items.Add(i)
+                Next
+                .Size = New Size(40, 21)
+                .Location = New Point(x2, Run(no - 1).cmbUnits.Location.Y + 30)
+                .Parent = Panel1
+            End With
+
+            .txtScore = New TextBox
+            With .txtScore
+                .Size = New Size(59, 20)
+                .Location = New Point(x3, Run(no - 1).txtScore.Location.Y + 30)
+                .Parent = Panel1
+                AddHandler .TextChanged, AddressOf ScoreChanged
+                AddHandler .CursorChanged, AddressOf ScoreChanged
+            End With
+
+
+            .cmbGrade = New ComboBox
+            With .cmbGrade
+                For i = 2 To 5 Step 0.5
+                    .Items.Add(i)
+                Next
+                .Size = New Size(40, 21)
+                .Location = New Point(x4, Run(no - 1).cmbGrade.Location.Y + 30)
+                .Parent = Panel1
+                'AddHandler .DropDownClosed, AddressOf GradeChanged
+            End With
+
+
+            .txtLetter = New TextBox
+            With .txtLetter
+                .ReadOnly = True
+                .Size = New Size(33, 20)
+                .Location = New Point(x5, Run(no - 1).txtLetter.Location.Y + 30)
+                .Parent = Panel1
+            End With
+        End With
+    End Sub
+
+    Public Function LetterFill()
+        For i = 0 To no
+            With Run(i)
+                If .txtName.Text <> "" Then
+                    If .txtScore.Text <> "" And Val(.txtScore.Text) <= 100 And Val(.txtScore.Text) >= 0 Then
+                        If Val(.txtScore.Text) >= 80 Or Val(.cmbGrade.Text) = 5 Then
+                            .txtLetter.Text = "A"
+                            .cmbGrade.Text = "5"
+                        ElseIf Val(.cmbGrade.Text) = 4.5 Then
+                            .txtLetter.Text = "B+"
+                            .cmbGrade.Text = "4.5"
+                        ElseIf Val(.txtScore.Text) >= 60 Or Val(.cmbGrade.Text) = 4 Then
+                            .txtLetter.Text = "B"
+                            .cmbGrade.Text = "4"
+                        ElseIf Val(.cmbGrade.Text) = 3.5 Then
+                            .txtLetter.Text = "C+"
+                            .cmbGrade.Text = "3.5"
+                        ElseIf Val(.txtScore.Text) >= 50 Or Val(.cmbGrade.Text) = 3 Then
+                            .txtLetter.Text = "C"
+                            .cmbGrade.Text = "3"
+                        ElseIf Val(.txtScore.Text) >= 45 Then
+                            .txtLetter.Text = "D"
+                            .cmbGrade.Text = "2"
+                        ElseIf Val(.txtScore.Text) >= 40 Then
+                            .txtLetter.Text = "E"
+                            .cmbGrade.Text = "1"
+                        ElseIf Val(.txtScore.Text) >= 0 Then
+                            .txtLetter.Text = "F"
+                            .cmbGrade.Text = "0"
+                        End If
+                    Else
+                        .txtScore.Clear()
+                        .cmbGrade.Text = ""
+                        'MsgBox("Value must be between 0 and 100", MsgBoxStyle.Exclamation, "Score Field Message")
+                    End If
+                End If
+            End With
+        Next
+    End Function
+
+    Private Sub ScoreChanged(sender As Object, e As EventArgs)
+        For i = 0 To no
+            With Run(i)
+                If .txtScore.Text = "" And .cmbGrade.Text = "" Then
+                    .txtLetter.Clear()
+                End If
+            End With
+            LetterFill()
+        Next
+    End Sub
+
+
+End Class
